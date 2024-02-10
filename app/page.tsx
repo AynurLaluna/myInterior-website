0eefb30ba62c292adff90fe440eb1b10e689f907
Aynur.Laluna <aynur.aqabalayeva52@gmail.com>
2024-02-10 13:27:06 +0400

    OK

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,7 @@ import React from 'react'
 
 function page() {
   return (
-    <div>
-      <h1>test</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, necessitatibus alias velit eos dolores blanditiis officiis culpa sint eaque aperiam at facilis eligendi commodi a laudantium numquam in fugiat molestias.
-      </p>
-    </div>
+    <div></div>
   )
 }
 
