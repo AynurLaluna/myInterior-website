4d20ecccc392d461aef8a188d23ccee4fa1b227b
Aynur.Laluna <aynur.aqabalayeva52@gmail.com>
2024-02-13 00:56:04 +0400

    page created

---
+++ app/(site)/about/page.tsx
@@ -0,0 +1,12 @@
+import Banner2 from '@/Components/Banner2'
+import React from 'react';
+
+const page = () => {
+  return (
+    <div>
+        <Banner2 header="About Us" title1="Home /" title2=" About Us" />
+    </div>
+  )
+}
+
+export default page;
