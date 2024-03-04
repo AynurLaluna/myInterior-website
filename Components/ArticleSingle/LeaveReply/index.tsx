e8b1cb60c5931888c9b3c2700ab88f542b904c61
Aynur.Laluna <aynur.aqabalayeva52@gmail.com>
2024-03-04 09:51:04 +0400

    add single article

--- Components/ArticleSingle/LeaveReply/index.tsx
+++ Components/ArticleSingle/LeaveReply/index.tsx
@@ -3,46 +3,53 @@ import React from "react";
 
 const LeaveReply = () => {
   return (
-    <div className="container mx-auto w-[88vw] my-20 md:my-32">
-      <h4 className="lg:text-2xl md:text-2xl sm:text-lg text-lg font-semibold font-[display] my-5 ">
+    <div className=" container md:mx-auto w-[64vw] my-20 md:my-32">
+      <h4 className=" md:text-2xl text-xl font-semibold font-[display] my-5 ">
         Leave a Reply
       </h4>
-      <div className="">
-        <div className="space-x-5">
-          <input
-            className="lg:w-[280px] border-b border-black py-2 font-[other] placeholder:text-main-4"
-            type="name"
-            placeholder="Name"
-            required
-          />
-          <input
-            className="lg:w-[280px] border-b border-black py-2 font-[other] placeholder:text-main-4"
-            type="email"
-            placeholder="Email"
-            required
-          />
-        </div>
-        <div className="space-x-5">
-          <input
-            className="lg:w-[280px] border-b border-black py-2 font-[other] placeholder:text-main-4"
-            type="text"
-            placeholder="Website"
-            required
-          />
-          <input
-            className="lg:w-[280px] border-b border-black py-2 font-[other] placeholder:text-main-4"
-            type="phone"
-            placeholder="Phone"
-            required
-          />
-        </div>
+      <form
+        action="https://getform.io/f/qalpmqra"
+        method="POST"
+        className="space-y-2 md:space-y-4 ">
         <input
-          className="w-96 lg:h-[10rem] md:h-[7rem] h-20 border-b font-[other] border-black placeholder:text-main-4"
+          type="text"
+          name="name"
+          className="w-full md:w-[46%] py-2   md:mr-4 border-b-[0.05rem] border-main-4  text-sm md:text-base font-[other] text-main-2 placeholder:text-main-4 "
+          placeholder="Name"
+          required
+        />
+        <input
+          type="email"
+          name="email"
+          className="w-full md:w-[46%] py-2  md:mr-4 border-b-[0.05rem] border-main-4  text-sm md:text-base font-[other] text-main-2 placeholder:text-main-4 "
+          placeholder="Email"
+          required
+        />
+
+        <input
+          type="text"
+          name="website"
+          placeholder="Website"
+          required
+          className="w-full md:w-[46%] py-2  md:mr-4 border-b-[0.05rem] border-main-4  text-sm md:text-base font-[other] text-main-2 placeholder:text-main-4 "
+        />
+
+        <input
+          type="tel"
+          name="phone"
+          placeholder="Phone"
+          required
+          className="w-full md:w-[46%] py-2  md:mr-4 border-b-[0.05rem] border-main-4  text-sm md:text-base font-[other] text-main-2 placeholder:text-main-4 "
+        />
+
+        <input
           type="textarea"
-          placeholder="Hello I am interested in.."
+          name="message"
+          className="border-b border-main-4  md:border-main-2 w-full h-24 md:h-32  text-sm md:text-base placeholder-main-4 font-[other]"
+          placeholder="Hello, I am interested in.."
         />
         <div className="pt-5 flex items-center space-x-2">
-          <input className="" type="checkbox" id="save" />
+          <input className="" type="checkbox" id="save" required name="save" />
           <label
             className="lg:text-base md:text-base sm:text-sm max-sm:text-sm font-[other] peer-disabled:cursor-not-allowed peer-disabled:opacity-70 text-main-1"
             htmlFor="save">
@@ -50,10 +57,11 @@ const LeaveReply = () => {
             comment.
           </label>
         </div>
-      </div>
-      <div className="mt-10">
-        <Button title="Send Now" to="#" />
-      </div>
+
+        <div className="pt-7 flex lg:justify-end md:justify-end justify-center">
+          <Button title="Send Now" to="https://getform.io/f/qalpmqra" />
+        </div>
+      </form>
     </div>
   );
 };
