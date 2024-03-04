dfb67dc631d28263fea2569a8e84f2adb0e742aa
Aynur.Laluna <aynur.aqabalayeva52@gmail.com>
2024-03-04 09:51:10 +0400

    add button

--- Components/Button2/index.tsx
+++ Components/Button2/index.tsx
@@ -7,9 +7,9 @@ function Button2({ title, to }: IButton) {
   return (
     <button
       type="submit"
-      className="lg:my-4 lg:px-7 md:px-6  px-4 py-2 flex item-center justify-center text-[#fff] bg-main-1 hover:bg-main-2 ease-in-out duration-300 rounded-[10px] text-center lg:text-lg md:text-base text-sm font-semibold font-[display] tracking-wider">
+      className="px-5 md:px-0 md:w-48 w-full py-2 flex item-center justify-center text-[#fff] bg-main-1 hover:bg-main-1 ease-in-out duration-300 rounded-[10px] text-center lg:text-lg md:text-base text-sm font-semibold font-[display] tracking-wider">
       <div className="flex items-center">
-        <Link className="hover:text-main-2" href={to}>
+        <Link className="text-white" href={to}>
           {title}
         </Link>
         <CgArrowRight className=" size-4 lg:ms-2 ms-1 text-main-2 hover:text-main-1 hover:opacity-100 ase-in-out duration-300 " />
