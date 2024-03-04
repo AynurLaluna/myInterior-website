459eab17c28a616deda37c5220417995d5f41dcc
Aynur.Laluna <aynur.aqabalayeva52@gmail.com>
2024-03-04 09:50:47 +0400

    brands added

--- Components/Brands/index.tsx
+++ Components/Brands/index.tsx
@@ -1,23 +1,49 @@
 import React from "react";
+import Image from "next/image";
 
 const Brands = () => {
   return (
-    <div className="container-sm mx-auto">
-      <div className="p-20 flex justify-center items-center gap-x-20">
-        <div className="w-12 h-12  ">
-          <img src="./brandLogos/01.svg" alt="logo" />
+    <div className="container mx-auto w-[88vw] mb-20">
+      <div className="p-20 flex justify-center items-center gap-10">
+        <div className="w-32 md:w-44">
+          <Image
+            src="./brandLogos/01.svg"
+            alt="logo"
+            width={100}
+            height={100}
+          />
         </div>
-        <div className="w-12 h-12  ">
-          <img src="./brandLogos/02.svg" alt="logo" />
+        <div className="w-32 md:w-44">
+          <Image
+            src="./brandLogos/02.svg"
+            alt="logo"
+            width={100}
+            height={100}
+          />
         </div>
-        <div className="w-12 h-12  ">
-          <img src="./brandLogos/03.svg" alt="logo" />
+        <div className="w-32 md:w-44">
+          <Image
+            src="./brandLogos/03.svg"
+            alt="logo"
+            width={100}
+            height={100}
+          />
         </div>
-        <div className="w-12 h-12  ">
-          <img src="./brandLogos/04.svg" alt="logo" />
+        <div className="w-32 md:w-44">
+          <Image
+            src="./brandLogos/04.svg"
+            alt="logo"
+            width={100}
+            height={100}
+          />
         </div>
-        <div className="w-12 h-12 ">
-          <img src="./brandLogos/05.svg" alt="logo" />
+        <div className="w-32 md:w-48">
+          <Image
+            src="./brandLogos/05.svg"
+            alt="logo"
+            width={100}
+            height={100}
+          />
         </div>
       </div>
     </div>
