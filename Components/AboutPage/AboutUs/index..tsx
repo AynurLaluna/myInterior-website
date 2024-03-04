3f18e46409ef1f1fbb95e135dab9339274995ee7
Aynur.Laluna <aynur.aqabalayeva52@gmail.com>
2024-03-04 09:49:43 +0400

    add about us

--- Components/AboutPage/AboutUs/index..tsx
+++ Components/AboutPage/AboutUs/index..tsx
@@ -14,28 +14,30 @@ const AboutUs = async () => {
       <div className="space-y-7 flex flex-col  aboutWrapper">
         {data.map(({ id, title, text, imageUrl }: IProps) => {
           return (
-            <div
-              key={id}
-              className=" flex flex-col-reverse md:flex-row justify-center items-center ">
-              <div className="flex flex-col space-y-3 md:space-y-5 w-full p-4">
-                <h3 className="text-2xl md:text-3xl lg:text-4xl font-bold font-[display] md:tracking-wide ">
-                  {title}
-                </h3>
-                <p className="text-base md:text-lg text-main-4 font-[other] ">
-                  {text}
-                </p>
-                <div className="">
-                  <Button to="/" title="Our Concept" />
+            <div className="aboutContainer">
+              <div
+                key={id}
+                className="flex flex-col-reverse md:flex-row justify-center items-center ">
+                <div className="flex flex-col space-y-3 md:space-y-5 w-full p-4">
+                  <h3 className="text-2xl md:text-3xl lg:text-4xl font-bold font-[display] md:tracking-wide ">
+                    {title}
+                  </h3>
+                  <p className="text-base md:text-lg text-main-4 font-[other] ">
+                    {text}
+                  </p>
+                  <div className="">
+                    <Button to="/" title="Our Concept" />
+                  </div>
                 </div>
-              </div>
-              <div className="w-full p-4 ">
-                <Image
-                  className="w-fit h-full"
-                  src={imageUrl}
-                  alt="interior "
-                  width={100}
-                  height={100}
-                />
+                <div className="w-full p-4 ">
+                  <Image
+                    className="w-fit h-full"
+                    src={imageUrl}
+                    alt="interior "
+                    width={100}
+                    height={100}
+                  />
+                </div>
               </div>
             </div>
           );
