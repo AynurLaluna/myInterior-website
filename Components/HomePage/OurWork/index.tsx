88a31a05552b85d7fc33aea31434effa5031e00e
Aynur.Laluna <aynur.aqabalayeva52@gmail.com>
2024-03-07 10:23:34 +0400

    changed

--- Components/HomePage/OurWork/index.tsx
+++ Components/HomePage/OurWork/index.tsx
@@ -22,16 +22,16 @@ function OurWork() {
 
   return (
     <div className="container mx-auto py-20 md:py-32 w-[88vw]">
-      <div className="flex flex-col md:flex-row md:justify-between items-center space-y-5 ">
+      <div className="flex flex-col md:flex-row lg:flex-row md:justify-between lg:justify-between items-center gap-10 ">
         {review.map(({ id, header, text }) => {
           return (
             <div
-              className="w-[320px] p-4 sm:border shadow-md md:border-0 rounded-xl text-center hover:bg-main-3 hover:shadow-xl hover:translate-y-2 ease-in-out duration-300 "
+              className="p-4 md:p-4 lg:p-5 sm:border shadow-md md:border-0 rounded-xl text-center hover:bg-main-3 hover:shadow-xl hover:translate-y-2 ease-in-out duration-300 "
               key={id}>
-              <h4 className="py-1 md:py-2 font-semibold lg:text-2xl md:text-xl leading-5 text-center text-main-2 font-[display]">
+              <h4 className="py-1 sm:py-1 md:py-2 lg:py-3 font-semibold lg:text-2xl md:text-xl sm:text-lg text-base text-center text-main-2 font-[display]">
                 {header}
               </h4>
-              <p className="text-center text-sm md:text-base text-main-4 font-[other]">
+              <p className="px-6 text-center text-sm sm:text-sm md:text-base lg:text-base text-main-4 font-[other]">
                 {text}
               </p>
               <IconButton to="/" title="Read More" />
--- Components/HomePage/WhoWeAre/index.tsx
+++ Components/HomePage/WhoWeAre/index.tsx
@@ -1,33 +1,32 @@
 import React from "react";
-import Button from "../../Button";
 import Link from "next/link";
 import Image from "next/image";
+import Button from "../../Button";
 import { IoCallOutline } from "react-icons/io5";
 
 function WhoWeAre() {
   return (
-    <div className="w-[88vw] container m-auto px-10">
-      <div className="my-10 flex flex-col-reverse md:flex-row  justify-between items-center">
-        <div className="flex flex-col items-stretch justify-center w-[500px] px-10">
-          <h2 className="text-3xl md:text-4xl lg:text-5xl pt-4 lg:leading-[62.5px] font-bold text-main-2">
+      <div className="container mx-auto w-[88vw] my-20 flex flex-col-reverse sm:flex-col-reverse md:flex-row lg:flex-row justify-between items-center md:gap-10 lg:gap-20">
+        <div className="md:w-1/2 lg:w-1/2 flex flex-col items-stretch justify-center">
+          <h2 className="pt-7 text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold text-main-2 sm:leading-8 md:leading-10 lg:leading-[62px]">
             We Create The Art Of Stylish Living Stylishly
           </h2>
-          <p className="my-8 text-sm md:text-base lg:text-large font-light text-main-4">
+          <p className="py-4 text-sm sm:text-sm md:text-base lg:text-base text-main-4">
             It is a long established fact that a reader will be distracted by
             the of readable content of a page when looking at its layouts the
             points of using that it has a more-or-less normal.
           </p>
           <div className="flex justify-start items-center ">
-            <div className="size-8 md:size-10 lg:size-12 flex justify-center items-center bg-[#F4F0EC] rounded-full">
+            <div className="size-10 sm:size-10 md:size-12 lg:size-14 flex justify-center items-center bg-[#F4F0EC] rounded-full hover:-translate-y-1 duration-300 ease-in-out">
               <Link href="/">
                 <IoCallOutline className="text-main-1" />
               </Link>
             </div>
-            <div className="text-sm md:text-base ms-2 flex flex-col font-[other]">
+            <div className="text-sm md:text-base lg:text-lg ms-2 flex flex-col font-[other]">
               <Link
-                className="text-main-2 text-base md:text-lg font-semibold tracking-wide"
+                className="text-main-2 text-base md:text-lg lg:text-xl font-semibold tracking-wider"
                 href="/">
-                012345678
+                <span>012345678</span>
               </Link>
               <span className="text-main-4 ">Call Us Anytime</span>
             </div>
@@ -46,7 +45,6 @@ function WhoWeAre() {
           />
         </div>
       </div>
-    </div>
   );
 }
 
